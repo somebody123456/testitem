b5812dffd8f3152d657d57c50ba0d4420ab81698
unknown <aivanov@AIVANOV.int.acumatica.com>
2014-10-30 16:00:34 +0300

    7

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0;i<100;i++)
+            {
+                Console.WriteLine(File.GetLastWriteTime(@"\\fs\DbBackUps\AutoTestStorage\Acumatica\5.00.2515\lin-mysql\api.tar").ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
